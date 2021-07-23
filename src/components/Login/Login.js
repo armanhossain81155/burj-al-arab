5e9c1231644375602b017d53d2b635c5c156dd8d
Arman Hossain <arman81155@gmail.com>
2021-07-23 23:02:37 +0600

    first commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,50 @@
-import React, { useContext } from 'react';
-
+import React, { useContext } from 'react'
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
-    );
-};
+    
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    
+    const  history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+    }
+    
+  const handleGoogleSignIn = () => {
+    var googleProvider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+     const  {displayName, email} = result.user
+     const signedInUser = {name: displayName, email}
+     setLoggedInUser(signedInUser)
+        history.replace(from);
 
-export default Login;
+      }).catch((error) => {
+
+      var errorCode = error.code
+      var errorMessage = error.message
+
+      var email = error.email
+
+      var credential = error.credential
+    })
+  }
+
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={handleGoogleSignIn}>
+        Sign in with google
+      </button>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCE8fW6cIMX1rC4wbwbNOGpFt0_z0QfVC8",
+    authDomain: "burj-al-arab-ebebe.firebaseapp.com",
+    projectId: "burj-al-arab-ebebe",
+    storageBucket: "burj-al-arab-ebebe.appspot.com",
+    messagingSenderId: "147474678125",
+    appId: "1:147474678125:web:278e8d96d3245b997c9a64"
+  };
+
+  export default firebaseConfig;
